bd7f6e9038d3736f4ebc194f0efaca44ee4147ac
mwoelke <m.woelke@hotmail.de>
2023-08-05 15:17:03 +0200

    make username configurable as a parameter

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 This custom mapper for keycloak allows querying a URL (e.g. REST API) and add the result as a claim.
 You can configure the URL, Username/Password for HTTP Basic Auth and a timeout in seconds.
+Optionally, you may also add the users username in a configurable URL parameter.
 
 If anything fails, "ERROR" will simply be put in the claim for now.
 
@@ -9,10 +10,16 @@ If anything fails, "ERROR" will simply be put in the claim for now.
 
 This has been tested with Keycloak 21 using the official Quarkus Container and OpenJDK 17.
 
+Compile the source using maven, then copy the resulting JAR into the `providers/` folder of your keycloak installation.
+
+Afterwards register the new provider using the keycloak CLI: `./kc.sh build`
+
 # Shoutouts
 
 This project is heavily based/inspired by the following repos:
 
 https://github.com/mschwartau/keycloak-custom-protocol-mapper-example
+
 https://github.com/dasniko/keycloak-tokenmapper-example
+
 https://github.com/tholst/keycloak-json-graphql-remote-claim
--- src/main/java/mwoelke/keycloak/restapimapper/RestApiMapper.java
+++ src/main/java/mwoelke/keycloak/restapimapper/RestApiMapper.java
@@ -1,5 +1,6 @@
 package mwoelke.keycloak.restapimapper;
 
+import org.apache.http.client.utils.URIBuilder;
 import org.keycloak.models.ClientSessionContext;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.ProtocolMapperModel;
@@ -13,6 +14,7 @@ import org.keycloak.provider.ProviderConfigProperty;
 import org.keycloak.representations.IDToken;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -20,10 +22,12 @@ import java.net.http.HttpResponse.BodyHandlers;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Base64;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class RestApiMapper extends AbstractOIDCProtocolMapper
-	implements OIDCAccessTokenMapper, OIDCIDTokenMapper, UserInfoTokenMapper {
+		implements OIDCAccessTokenMapper, OIDCIDTokenMapper, UserInfoTokenMapper {
 
 	public static final String PROVIDER_ID = "oidc-rest-api-mapper";
 
@@ -31,14 +35,33 @@ public class RestApiMapper extends AbstractOIDCProtocolMapper
 
 	static final String API_URL = "apiUrl";
 	static final String API_USERNAME = "apiUsername";
-    static final String API_PASSWORD = "apiPassword";
-    static final String API_TIMEOUT = "apiTimout";
+	static final String API_PASSWORD = "apiPassword";
+	static final String API_TIMEOUT = "apiTimout";
+	static final String USER_ADD_NAME = "userAddName";
+	static final String USER_NAME_PARAMETER = "userNameParameter";
 
 	static {
-		configProperties.add(new ProviderConfigProperty(API_URL, "API URL", "URL of the REST API.", ProviderConfigProperty.STRING_TYPE, "https://example.org/api/endpoint"));
-		configProperties.add(new ProviderConfigProperty(API_USERNAME, "API Username", "Username for HTTP Auth.", ProviderConfigProperty.STRING_TYPE, "admin"));
-        configProperties.add(new ProviderConfigProperty(API_PASSWORD, "API Password", "Password for HTTP Auth.", ProviderConfigProperty.PASSWORD, "pass"));
-        configProperties.add(new ProviderConfigProperty(API_TIMEOUT, "Timeout", "Timeout in seconds.", ProviderConfigProperty.STRING_TYPE, 3));
+		// URL Field
+		configProperties.add(new ProviderConfigProperty(API_URL, "API URL", "URL of the REST API.",
+				ProviderConfigProperty.STRING_TYPE, "https://example.org/api/endpoint"));
+		// HTTP Auth username Field
+		configProperties.add(new ProviderConfigProperty(API_USERNAME, "API Username", "Username for HTTP Auth.",
+				ProviderConfigProperty.STRING_TYPE, "admin"));
+		// HTTP Auth Password field
+		configProperties.add(new ProviderConfigProperty(API_PASSWORD, "API Password", "Password for HTTP Auth.",
+				ProviderConfigProperty.PASSWORD, "pass"));
+		// Timeout field
+		configProperties.add(new ProviderConfigProperty(API_TIMEOUT, "Timeout", "Timeout in seconds.",
+				ProviderConfigProperty.STRING_TYPE, 3));
+
+		// Add username Checkbox
+		configProperties.add(new ProviderConfigProperty(USER_ADD_NAME, "Add username?",
+				"Should the username be added to the query?", ProviderConfigProperty.BOOLEAN_TYPE, true));
+
+		// Username paramter name
+		configProperties.add(new ProviderConfigProperty(USER_NAME_PARAMETER, "Username parameter",
+				"Name of the parameter the username will get send with. Only takes effect if \"add username\" is enabled.",
+				ProviderConfigProperty.STRING_TYPE, "username"));
 
 		OIDCAttributeMapperHelper.addTokenClaimNameConfig(configProperties);
 		OIDCAttributeMapperHelper.addIncludeInTokensConfig(configProperties, RestApiMapper.class);
@@ -71,43 +94,59 @@ public class RestApiMapper extends AbstractOIDCProtocolMapper
 
 	@Override
 	protected void setClaim(
-		IDToken token, 
-		ProtocolMapperModel mappingModel, 
-		UserSessionModel userSession, 
-		KeycloakSession keycloakSession, 
-		ClientSessionContext clientSessionCtx
-	) {
-        String apiUrl = mappingModel.getConfig().get(API_URL);
-        String apiUsername = mappingModel.getConfig().get(API_USERNAME);
-		String apiPassword = mappingModel.getConfig().get(API_PASSWORD);
-		int timeout = Integer.parseInt(mappingModel.getConfig().get(API_TIMEOUT));
+			IDToken token,
+			ProtocolMapperModel mappingModel,
+			UserSessionModel userSession,
+			KeycloakSession keycloakSession,
+			ClientSessionContext clientSessionCtx) {
+		// get config
+		final String apiUrl = mappingModel.getConfig().get(API_URL);
+		final String apiUsername = mappingModel.getConfig().get(API_USERNAME);
+		final String apiPassword = mappingModel.getConfig().get(API_PASSWORD);
+		final int timeout = Integer.parseInt(mappingModel.getConfig().get(API_TIMEOUT));
+		final boolean addUsername = Boolean.parseBoolean(mappingModel.getConfig().get(USER_ADD_NAME));
+		final String usernameParameter = mappingModel.getConfig().get(USER_NAME_PARAMETER);
 
+		// result string
 		String res;
 
-		//create client
+		// create client
 		HttpClient client = HttpClient.newHttpClient();
 
-		//make request
 		try {
+			// build URI
+			URIBuilder uriBuilder = new URIBuilder(apiUrl);
+
+			// add username to parameter if config is selected and username is not empty
+			if (addUsername && !"".equals(usernameParameter)) {
+				uriBuilder.addParameter(usernameParameter, userSession.getLoginUsername());
+			}
+
+			// make request
 			HttpRequest getRequest = HttpRequest.newBuilder()
-				.uri(new URI(apiUrl))
-				.header("Authorization", getBasicAuthHeader(apiUsername, apiPassword))
-				.timeout(Duration.ofSeconds(Long.valueOf(timeout)))
-				.GET()
-				.build();
+					.uri(new URI(uriBuilder.toString()))
+					.header("Authorization", getBasicAuthHeader(apiUsername, apiPassword))
+					.timeout(Duration.ofSeconds(Long.valueOf(timeout)))
+					.GET()
+					.build();
 
 			HttpResponse<String> response = client.send(getRequest, BodyHandlers.ofString());
 			res = response.statusCode() == 200 ? response.body() : "ERROR";
-
-			//res = response.body();
 		} catch (Exception e) {
-			//hardmap to ERROR on any exception (timeout, invalid url, ...)
+			// hardmap to ERROR on any exception (timeout, invalid url, ...)
 			res = "ERROR";
 		}
 
 		OIDCAttributeMapperHelper.mapClaim(token, mappingModel, res);
 	}
 
+	/**
+	 * Encode http auth username and password to base64
+	 * 
+	 * @param username
+	 * @param password
+	 * @return
+	 */
 	private static final String getBasicAuthHeader(String username, String password) {
 		String clearText = username + ":" + password;
 		return "Basic " + Base64.getEncoder().encodeToString(clearText.getBytes());
